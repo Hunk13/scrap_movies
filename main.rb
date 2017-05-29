bd3dd1a10dcef3f4fe32cd46c2289067b73ac6bc
Aliaksandr Kalinka <alex.kalinoff@gmail.com>
2017-05-29 19:41:58 +0300

    Correction of comments

--- main.rb
+++ main.rb
@@ -2,29 +2,20 @@
 file = 'movies.txt'
 search_text = 'Max'
 
-if ARGV.length.zero?
-  puts 'There is no input file. Take the default file.'
-  file
-elsif ARGV.length > 1
-  abort('Need only one parameter - file name.')
-elsif File.exist?(ARGV[0]) ? file = ARGV[0] : abort("File '#{ARGV[0]}' not exist.")
+abort('Need only one parameter - file name.') if ARGV.length > 1
+
+if File.exist?(file)
+  file if ARGV[0] == file || nil?
+elsif ARGV[0] && File.exist?(ARGV[0])
+  file = ARGV[0]
 else
-  file
+  ARGV[0] ? abort("File '#{ARGV[0]}' not exist.") : abort('No files to scrap')
 end
 
-open_file = File.open(file, 'r')
-output_movies = []
-
-open_file.each do |line|
+File.open(file).each_line do |line|
   split_line = line.split('|')
-  output_movies << split_line if split_line[1].include? search_text
+  puts split_line[1] + ' | ' + ('*' * (split_line[7].to_f * 10 - 80).to_i) if split_line[1].include? search_text
 end
-
-output_movies.each do |output_movie|
-  puts output_movie[1] + ' | ' + ('*' * ('%.1f' % ((output_movie[7].to_f - 8) * 10)).to_i)
-end
-
-open_file.close
 
 # output
 #
