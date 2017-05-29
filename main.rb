bcd8e004a88a759152aac96cc08ceb474dbf623b
Aliaksandr Kalinka <alex.kalinoff@gmail.com>
2017-05-29 23:51:46 +0300

    refactoring of code

--- main.rb
+++ main.rb
@@ -1,16 +1,11 @@
 # second lesson
-file = 'movies.txt'
+scrap_file = 'movies.txt'
 search_text = 'Max'
 
 abort('Need only one parameter - file name.') if ARGV.length > 1
 
-if File.exist?(file)
-  file if ARGV[0] == file || nil?
-elsif ARGV[0] && File.exist?(ARGV[0])
-  file = ARGV[0]
-else
-  ARGV[0] ? abort("File '#{ARGV[0]}' not exist.") : abort('No files to scrap')
-end
+file = ARGV[0] || scrap_file
+abort("File '#{ARGV[0]}' not exist.") unless File.exist?(file)
 
 File.open(file).each_line do |line|
   split_line = line.split('|')
