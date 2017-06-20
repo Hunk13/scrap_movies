03a29645ab357641eddf9b54035e04914b95af1c
Aliaksandr Kalinka <alex.kalinoff@gmail.com>
2017-06-20 13:29:30 +0300

    fix Shadowing outer local variable

--- netflix.rb
+++ netflix.rb
@@ -8,13 +8,13 @@ class Netflix < MovieCollection
   end
 
   def show(params)
-    movie = filter(params).sort_by { |movie| movie.rating * rand }.last
-    raise("Film Not Found") if filter(params).empty?
-    raise("Not enough money. This movie cost #{movie.movie_price}. Your balance #{@money}") if @money < movie.movie_price
+    mov = filter(params).sort_by { |movie| movie.rating * rand }.last
+    raise('Film Not Found') if filter(params).empty?
+    raise("Not enough money. This movie cost #{mov.movie_price}. Your balance #{@money}") if @money < mov.movie_price
     start_time = Time.now
-    end_time = start_time + movie.length * 60
-    @money -= movie.movie_price
-    puts "Now showing: #{movie.title} #{start_time.strftime('%H:%M:%S')} - #{end_time.strftime('%H:%M:%S')}"
+    end_time = start_time + mov.length * 60
+    @money -= mov.movie_price
+    puts "Now showing: #{mov.title} #{start_time.strftime('%H:%M:%S')} - #{end_time.strftime('%H:%M:%S')}"
   end
 
   def how_much?(input_movie_name)
